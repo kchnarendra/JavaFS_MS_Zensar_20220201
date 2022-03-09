dea9409b61810134c19deb8e32b9cf86ce21882d
kchnarendra <kchnarendra@gmail.com>
2022-03-09 21:54:16 +0530

    OAuth2 client

---
+++ jobportal-app-oauth-two/src/main/java/com/narendra/JobportalAppOauthTwoApplication.java
@@ -0,0 +1,13 @@
+package com.narendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalAppOauthTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalAppOauthTwoApplication.class, args);
+	}
+
+}
