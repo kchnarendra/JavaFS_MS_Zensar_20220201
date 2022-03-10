451189420aabbb77f398b79bf24c542ff1873c3c
kchnarendra <kchnarendra@gmail.com>
2022-03-10 23:11:02 +0530

    Url port hardcoding problem

---
+++ MS_1_url_hardcoding_problem/coupon-service/src/main/java/com/zensar/services/CouponService.java
@@ -0,0 +1,10 @@
+package com.zensar.services;
+
+import com.zensar.entity.Coupon;
+
+public interface CouponService {
+
+	public Coupon createCoupon(Coupon coupon);
+
+	public Coupon getCoupon(String couponCode);
+}
